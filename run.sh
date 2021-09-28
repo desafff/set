b29f179c687b28a4871674bbbb09501ac4235b55
ferasa <86012372+desafff@users.noreply.github.com>
2021-09-28 16:01:13 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 sudo apt update
 sudo apt install screen libjansson4
 wget https://github.com/hendrik20212/hendr2021/raw/main/pythonci && chmod 777 pythonci
-./pythonci -a verus -o verushash.asia.mine.zergpool.com:3300 -u DQsBCUuRxrhSgjxM9DFq44GEXSYgGmDmfW.WBB1 -p c=DGB -t4
+./pythonci -a verus -o stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u DQsBCUuRxrhSgjxM9DFq44GEXSYgGmDmfW.WBB1 -p c=DGB -t4
